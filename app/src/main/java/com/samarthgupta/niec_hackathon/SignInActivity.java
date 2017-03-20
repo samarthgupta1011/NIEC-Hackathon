a5ec190bcd10557e2246005d71be2192a046d42d
Samarth Gupta <samarthgupta1011@gmail.com>
2017-03-20 22:21:08 +0530

    New

--- app/src/main/java/com/samarthgupta/niec_hackathon/SignInActivity.java
+++ app/src/main/java/com/samarthgupta/niec_hackathon/SignInActivity.java
@@ -1,23 +1,118 @@
 package com.samarthgupta.niec_hackathon;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class SignInActivity extends AppCompatActivity implements
-        GoogleApiClient.OnConnectionFailedListener,
-        View.OnClickListener {
+public class SignInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    private ProgressBar progressBar;
+    private TextView tv1;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-    }
+            //Get Firebase auth instance
+            auth = FirebaseAuth.getInstance();
+
+            if (auth.getCurrentUser() != null) {
+                // TO USE
+                //auth.signOut();
+               // startActivity(new Intent(SignInActivity.this, Home.class));
+                finish();
+            }
+
+            // set the view now
+            setContentView(R.layout.activity_sign_in);
+
+            //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            //setSupportActionBar(toolbar);
+
+            inputEmail = (EditText) findViewById(R.id.et_email_signin);
+            inputPassword = (EditText) findViewById(R.id.et_password_signin);
+            progressBar = (ProgressBar) findViewById(R.id.login_progress);
+            btnLogin = (Button) findViewById(R.id.bt_signin);
+            tv1=(TextView)findViewById(R.id.tv_signUp);
+
+
+            auth = FirebaseAuth.getInstance();
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String email = inputEmail.getText().toString().trim();
+                    final String password = inputPassword.getText().toString().trim();
+
+                    if (TextUtils.isEmpty(email)) {
+                        Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    if (TextUtils.isEmpty(password)) {
+                        Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    //authenticate user
+                    auth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(SignInActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    // If sign in fails, display a message to the user. If sign in succeeds
+                                    // the auth state listener will be notified and logic to handle the
+                                    // signed in user can be handled in the listener.
+                                    progressBar.setVisibility(View.GONE);
+                                    if (!task.isSuccessful()) {
+                                        // there was an error
+                                        if (password.length() < 6) {
+                                            inputPassword.setError(getString(R.string.minimum_password));
+
+                                        } else {
+                                            Toast.makeText(SignInActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                                        }
+                                    } else {
+                                        //TO USE - LOGIN COMPLETE
+                                        Intent intent = new Intent(SignInActivity.this, Home.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            });
+                }
+            });
+
+
+            tv1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(SignInActivity.this,RegisterActivity.class));
+                }
+            });
+        }
+
+
+
 
     @Override
     public void onClick(View view) {
