ba0a864f9163a6e8887cad4cc98178f211b56c2d
Samarth Gupta <samarthgupta1011@gmail.com>
2017-03-21 00:55:33 +0530

    Sarthak gmail theek kr

--- app/src/main/java/com/samarthgupta/niec_hackathon/SignInActivity.java
+++ app/src/main/java/com/samarthgupta/niec_hackathon/SignInActivity.java
@@ -1,10 +1,12 @@
 package com.samarthgupta.niec_hackathon;
 
 import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,8 +14,15 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -26,6 +35,9 @@ public class SignInActivity extends AppCompatActivity implements GoogleApiClient
     private ProgressBar progressBar;
     private TextView tv1;
     private Button btnLogin;
+    GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 9001;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +50,9 @@ public class SignInActivity extends AppCompatActivity implements GoogleApiClient
             if (auth.getCurrentUser() != null) {
                 // TO USE
                 //auth.signOut();
+
+
+                //////////////////////////////////
                // startActivity(new Intent(SignInActivity.this, Home.class));
                 finish();
             }
@@ -93,8 +108,9 @@ public class SignInActivity extends AppCompatActivity implements GoogleApiClient
                                         }
                                     } else {
                                         //TO USE - LOGIN COMPLETE
-                                        Intent intent = new Intent(SignInActivity.this, Home.class);
-                                        startActivity(intent);
+                                        //////////////////////////////////
+                                      //  Intent intent = new Intent(SignInActivity.this, Home.class);
+                                     //   startActivity(intent);
                                         finish();
                                     }
                                 }
@@ -109,18 +125,107 @@ public class SignInActivity extends AppCompatActivity implements GoogleApiClient
                     startActivity(new Intent(SignInActivity.this,RegisterActivity.class));
                 }
             });
-        }
 
 
+        //GOOGLE SIGN IN
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
+         mGoogleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        SignInButton signInButton = (SignInButton) findViewById(R.id.bt_google_signin);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setOnClickListener(this);
+
+    }
 
     @Override
     public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.bt_google_signin:
+                signIn();
+                break; }
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
 
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("TAG", "onConnectionFailed:" + connectionResult);
+
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly.
+            Log.d("TAG", "Got cached sign-in");
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d("TAG", "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            ///////////////////////
+            Log.i("TAG",acct.getDisplayName());
+            Log.i("TAG",acct.getEmail());
+            //SEND DATA TO FIREBASE
+         //   mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+           updateUI(false);
+        }
+    }
+
+  private void updateUI(boolean signedIn) {
+        if (signedIn) {
+            findViewById(R.id.bt_google_signin).setVisibility(View.GONE);
+        } else {
+            findViewById(R.id.bt_google_signin).setVisibility(View.VISIBLE);
+        }
+    }
+
+
+
 }
