b67b025754a4f5dc6481e88ae4a3e399a1acc76b
Samarth Gupta <samarthgupta1011@gmail.com>
2017-03-20 21:29:33 +0530

    New

--- app/src/main/java/com/samarthgupta/niec_hackathon/MainActivity.java
+++ app/src/main/java/com/samarthgupta/niec_hackathon/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                
             }
         });
 
---
+++ app/src/main/java/com/samarthgupta/niec_hackathon/POJO/UserInformation.java
@@ -0,0 +1,61 @@
+package com.samarthgupta.niec_hackathon.POJO;
+
+/**
+ * Created by samarthgupta on 13/02/17.
+ */
+
+public class UserInformation {
+
+    private   String email;
+    private String password;
+    private String mno;
+    private String city;
+    private String fname;
+    private String lname;
+    private String icode;
+
+    public UserInformation() {
+        //Empty constructor
+    }
+
+    public UserInformation(String email, String password , String fname, String lname, String mno, String city, String icode){
+
+        this.email = email;
+        this.password = password;
+        this.city = city;
+        this.fname = fname;
+        this.lname = lname;
+        this.mno = mno;
+        this.icode = icode;
+
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getIcode() {
+        return icode;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getMno() {
+        return mno;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
